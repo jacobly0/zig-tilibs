732d615a7b28c7883e77e53a8e8af8752989dd6f
Jacob Young <jacobly0@users.noreply.github.com>
2024-04-10 05:11:58 -0400

    update to zig master

--- build.zig
+++ build.zig
@@ -1,18 +1,19 @@
 const std = @import("std");
 
-pub fn defineOsMacros(step: *std.Build.Step.Compile, target: std.Target) void {
+pub fn defineOsMacros(mod: *std.Build.Module) void {
+    const target = mod.resolved_target.?.result;
     switch (target.os.tag) {
-        .linux => step.defineCMacro("__LINUX__", null),
-        .macos => step.defineCMacro("__MACOSX__", null),
-        .windows => step.defineCMacro("__WIN32__", null),
+        .linux => mod.addCMacro("__LINUX__", "1"),
+        .macos => mod.addCMacro("__MACOSX__", "1"),
+        .windows => mod.addCMacro("__WIN32__", "1"),
         else => if (target.os.tag.isBSD()) {
-            step.defineCMacro("__BSD__", null);
+            mod.addCMacro("__BSD__", "1");
         } else {
             std.log.err("Unsupported OS: {}", .{target});
-            step.step.owner.invalid_user_input = true;
+            mod.owner.invalid_user_input = true;
         },
     }
-    if (target.isMinGW()) step.defineCMacro("__MINGW32__", null);
+    if (target.isMinGW()) mod.addCMacro("__MINGW32__", "1");
 }
 
 pub fn build(b: *std.Build) void {
@@ -26,80 +27,76 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    libticonv.linkLibCpp();
-    libticonv.linkSystemLibrary("glib-2.0");
-    libticonv.defineCMacro("restrict", "__restrict");
-    libticonv.defineCMacro("TICONV_EXPORTS", "");
-    libticonv.defineCMacro("VERSION", "\"1.1.6\"");
-    defineOsMacros(libticonv, target.result);
-    libticonv.addCSourceFiles(.{
-        .dependency = upstream,
+    libticonv.root_module.link_libcpp = true;
+    libticonv.root_module.linkSystemLibrary("glib-2.0", .{});
+    libticonv.root_module.addCMacro("restrict", "__restrict");
+    libticonv.root_module.addCMacro("TICONV_EXPORTS", "");
+    libticonv.root_module.addCMacro("VERSION", "\"1.1.6\"");
+    defineOsMacros(&libticonv.root_module);
+    libticonv.root_module.addCSourceFiles(.{
+        .root = upstream.path("libticonv/trunk/src"),
         .files = &.{
-            "libticonv/trunk/src/charset.cc",
-            "libticonv/trunk/src/filename.cc",
-            "libticonv/trunk/src/iconv.c",
-            "libticonv/trunk/src/ticonv.cc",
-            "libticonv/trunk/src/tokens.cc",
-            "libticonv/trunk/src/type2str.cc",
+            "charset.cc",
+            "filename.cc",
+            "iconv.c",
+            "ticonv.cc",
+            "tokens.cc",
+            "type2str.cc",
         },
     });
-    libticonv.installHeadersDirectoryOptions(.{
-        .source_dir = upstream.path("libticonv/trunk/src"),
-        .install_dir = .header,
-        .install_subdir = "",
+    libticonv.installHeadersDirectory(upstream.path("libticonv/trunk/src"), "", .{
         .include_extensions = &.{
             "charset.h",
             "export4.h",
             "ticonv.h",
         },
     });
-    b.installArtifact(libticonv);
+    const libticonv_install = b.addInstallArtifact(libticonv, .{});
+    b.step("ticonv", "Build libticonv").dependOn(&libticonv_install.step);
+    b.getInstallStep().dependOn(&libticonv_install.step);
 
     const libtifiles2 = b.addStaticLibrary(.{
         .name = "tifiles2",
         .target = target,
         .optimize = optimize,
     });
-    libtifiles2.linkLibCpp();
-    libtifiles2.linkSystemLibrary("glib-2.0");
-    libtifiles2.linkLibrary(libarchive.artifact("archive"));
-    libtifiles2.linkLibrary(libticonv);
-    libtifiles2.defineCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
+    libtifiles2.root_module.link_libcpp = true;
+    libtifiles2.root_module.linkSystemLibrary("glib-2.0", .{});
+    libtifiles2.root_module.linkLibrary(libarchive.artifact("archive"));
+    libtifiles2.root_module.linkLibrary(libticonv);
+    libtifiles2.root_module.addCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
         b.getInstallPath(.{ .custom = "share" }, "locale"),
     }));
-    libtifiles2.defineCMacro("restrict", "__restrict");
-    libtifiles2.defineCMacro("VERSION", "\"1.1.8\"");
-    defineOsMacros(libtifiles2, target.result);
-    libtifiles2.addCSourceFiles(.{
-        .dependency = upstream,
+    libtifiles2.root_module.addCMacro("restrict", "__restrict");
+    libtifiles2.root_module.addCMacro("VERSION", "\"1.1.8\"");
+    defineOsMacros(&libtifiles2.root_module);
+    libtifiles2.root_module.addCSourceFiles(.{
+        .root = upstream.path("libtifiles/trunk/src"),
         .files = &.{
-            "libtifiles/trunk/src/cert.cc",
-            "libtifiles/trunk/src/comments.cc",
-            "libtifiles/trunk/src/error.cc",
-            "libtifiles/trunk/src/files8x.cc",
-            "libtifiles/trunk/src/files9x.cc",
-            "libtifiles/trunk/src/filesnsp.cc",
-            "libtifiles/trunk/src/filesxx.cc",
-            "libtifiles/trunk/src/filetypes.cc",
-            "libtifiles/trunk/src/grouped.cc",
-            "libtifiles/trunk/src/intelhex.cc",
-            "libtifiles/trunk/src/misc.cc",
-            "libtifiles/trunk/src/rwfile.cc",
-            "libtifiles/trunk/src/tifiles.cc",
-            "libtifiles/trunk/src/tigroup.cc",
-            "libtifiles/trunk/src/type2str.cc",
-            "libtifiles/trunk/src/types68k.cc",
-            "libtifiles/trunk/src/types83p.cc",
-            "libtifiles/trunk/src/typesnsp.cc",
-            "libtifiles/trunk/src/typesoldz80.cc",
-            "libtifiles/trunk/src/typesxx.cc",
-            "libtifiles/trunk/src/ve_fp.cc",
+            "cert.cc",
+            "comments.cc",
+            "error.cc",
+            "files8x.cc",
+            "files9x.cc",
+            "filesnsp.cc",
+            "filesxx.cc",
+            "filetypes.cc",
+            "grouped.cc",
+            "intelhex.cc",
+            "misc.cc",
+            "rwfile.cc",
+            "tifiles.cc",
+            "tigroup.cc",
+            "type2str.cc",
+            "types68k.cc",
+            "types83p.cc",
+            "typesnsp.cc",
+            "typesoldz80.cc",
+            "typesxx.cc",
+            "ve_fp.cc",
         },
     });
-    libtifiles2.installHeadersDirectoryOptions(.{
-        .source_dir = upstream.path("libtifiles/trunk/src"),
-        .install_dir = .header,
-        .install_subdir = "",
+    libtifiles2.installHeadersDirectory(upstream.path("libtifiles/trunk/src"), "", .{
         .include_extensions = &.{
             "tifiles.h",
             "export2.h",
@@ -121,130 +118,125 @@ pub fn build(b: *std.Build) void {
             "typesxx.h",
         },
     });
-    b.installArtifact(libtifiles2);
+    const libtifiles2_install = b.addInstallArtifact(libtifiles2, .{});
+    b.step("tifiles2", "Build libtifiles2").dependOn(&libtifiles2_install.step);
+    b.getInstallStep().dependOn(&libtifiles2_install.step);
 
     const libticables2 = b.addStaticLibrary(.{
         .name = "ticables2",
         .target = target,
         .optimize = optimize,
     });
-    libticables2.linkLibCpp();
-    libticables2.linkSystemLibrary("glib-2.0");
-    libticables2.linkSystemLibrary("libusb-1.0");
-    libticables2.addIncludePath(
-        upstream.path("libticables/trunk/src"),
-    );
-    libticables2.defineCMacro("HAVE_LIBUSB_1_0", "1");
-    libticables2.defineCMacro("HAVE_LIBUSB10_STRERROR", "1");
-    libticables2.defineCMacro("HAVE_TERMIOS_H", "1");
-    libticables2.defineCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
+    libticables2.root_module.link_libcpp = true;
+    libticables2.root_module.linkSystemLibrary("glib-2.0", .{});
+    libticables2.root_module.linkSystemLibrary("libusb-1.0", .{});
+    libticables2.root_module.addCMacro("HAVE_LIBUSB_1_0", "1");
+    libticables2.root_module.addCMacro("HAVE_LIBUSB10_STRERROR", "1");
+    libticables2.root_module.addCMacro("HAVE_TERMIOS_H", "1");
+    libticables2.root_module.addCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
         b.getInstallPath(.{ .custom = "share" }, "locale"),
     }));
-    libticables2.defineCMacro("VERSION", "\"1.3.6\"");
-    defineOsMacros(libticables2, target.result);
-    libticables2.addCSourceFiles(.{
-        .dependency = upstream,
+    libticables2.root_module.addCMacro("VERSION", "\"1.3.6\"");
+    defineOsMacros(&libticables2.root_module);
+    libticables2.root_module.addCSourceFiles(.{
+        .root = upstream.path("libticables/trunk/src"),
         .files = &.{
-            "libticables/trunk/src/data_log.cc",
-            "libticables/trunk/src/detect.cc",
-            "libticables/trunk/src/error.cc",
-            "libticables/trunk/src/hex2dbus.cc",
-            "libticables/trunk/src/hex2dusb.cc",
-            "libticables/trunk/src/hex2nsp.cc",
-            "libticables/trunk/src/ioports.cc",
-            "libticables/trunk/src/link_blk.cc",
-            "libticables/trunk/src/link_gry.cc",
-            "libticables/trunk/src/link_nul.cc",
-            "libticables/trunk/src/link_par.cc",
-            "libticables/trunk/src/link_tcpc.cc",
-            "libticables/trunk/src/link_tcps.cc",
-            "libticables/trunk/src/link_tie.cc",
-            "libticables/trunk/src/link_usb.cc",
-            "libticables/trunk/src/link_vti.cc",
-            "libticables/trunk/src/link_xxx.cc",
-            "libticables/trunk/src/log_dbus.cc",
-            "libticables/trunk/src/log_dusb.cc",
-            "libticables/trunk/src/log_hex.cc",
-            "libticables/trunk/src/log_nsp.cc",
-            "libticables/trunk/src/none.cc",
-            "libticables/trunk/src/probe.cc",
-            "libticables/trunk/src/ticables.cc",
-            "libticables/trunk/src/type2str.cc",
+            "data_log.cc",
+            "detect.cc",
+            "error.cc",
+            "hex2dbus.cc",
+            "hex2dusb.cc",
+            "hex2nsp.cc",
+            "ioports.cc",
+            "link_blk.cc",
+            "link_gry.cc",
+            "link_nul.cc",
+            "link_par.cc",
+            "link_tcpc.cc",
+            "link_tcps.cc",
+            "link_tie.cc",
+            "link_usb.cc",
+            "link_vti.cc",
+            "link_xxx.cc",
+            "log_dbus.cc",
+            "log_dusb.cc",
+            "log_hex.cc",
+            "log_nsp.cc",
+            "none.cc",
+            "probe.cc",
+            "ticables.cc",
+            "type2str.cc",
         },
     });
-    libticables2.installHeadersDirectoryOptions(.{
-        .source_dir = upstream.path("libticables/trunk/src"),
-        .install_dir = .header,
-        .install_subdir = "",
+    libticables2.installHeadersDirectory(upstream.path("libticables/trunk/src"), "", .{
         .include_extensions = &.{
             "ticables.h",
             "export1.h",
             "timeout.h",
         },
     });
-    b.installArtifact(libticables2);
+    const libticables2_install = b.addInstallArtifact(libticables2, .{});
+    b.step("ticables2", "Build libticables2").dependOn(&libticables2_install.step);
+    b.getInstallStep().dependOn(&libticables2_install.step);
 
     const libticalcs2 = b.addStaticLibrary(.{
         .name = "ticalcs2",
         .target = target,
         .optimize = optimize,
     });
-    libticalcs2.linkLibC();
-    libticalcs2.linkLibrary(libticonv);
-    libticalcs2.linkLibrary(libtifiles2);
-    libticalcs2.linkLibrary(libticables2);
-    libticalcs2.defineCMacro("HAVE_ASCTIME_R", "1");
-    libticalcs2.defineCMacro("HAVE_CTIME_R", "1");
-    libticalcs2.defineCMacro("HAVE_LOCALTIME_R", "1");
-    libticalcs2.defineCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
+    libticalcs2.root_module.link_libcpp = true;
+    libticalcs2.root_module.linkLibrary(libticonv);
+    libticalcs2.root_module.linkLibrary(libtifiles2);
+    libticalcs2.root_module.linkLibrary(libticables2);
+    libticalcs2.root_module.addCMacro("HAVE_ASCTIME_R", "1");
+    libticalcs2.root_module.addCMacro("HAVE_CTIME_R", "1");
+    libticalcs2.root_module.addCMacro("HAVE_LOCALTIME_R", "1");
+    libticalcs2.root_module.addCMacro("LOCALEDIR", b.fmt("\"{s}\"", .{
         b.getInstallPath(.{ .custom = "share" }, "locale"),
     }));
-    libticalcs2.defineCMacro("restrict", "__restrict");
-    libticalcs2.defineCMacro("VERSION", "\"1.1.10\"");
-    defineOsMacros(libticalcs2, target.result);
-    libticalcs2.addCSourceFiles(.{
-        .dependency = upstream,
+    libticalcs2.root_module.addCMacro("restrict", "__restrict");
+    libticalcs2.root_module.addCMacro("VERSION", "\"1.1.10\"");
+    defineOsMacros(&libticalcs2.root_module);
+    libticalcs2.root_module.addCSourceFiles(.{
+        .root = upstream.path("libticalcs/trunk/src"),
         .files = &.{
-            "libticalcs/trunk/src/backup.cc",
-            "libticalcs/trunk/src/calc_00.cc",
-            "libticalcs/trunk/src/calc_73.cc",
-            "libticalcs/trunk/src/calc_84p.cc",
-            "libticalcs/trunk/src/calc_89t.cc",
-            "libticalcs/trunk/src/calc_8x.cc",
-            "libticalcs/trunk/src/calc_9x.cc",
-            "libticalcs/trunk/src/calc_nsp.cc",
-            "libticalcs/trunk/src/calc_xx.cc",
-            "libticalcs/trunk/src/clock.cc",
-            "libticalcs/trunk/src/cmd68k.cc",
-            "libticalcs/trunk/src/cmdz80.cc",
-            "libticalcs/trunk/src/dbus_pkt.cc",
-            "libticalcs/trunk/src/dirlist.cc",
-            "libticalcs/trunk/src/dusb_cmd.cc",
-            "libticalcs/trunk/src/dusb_rpkt.cc",
-            "libticalcs/trunk/src/dusb_vpkt.cc",
-            "libticalcs/trunk/src/error.cc",
-            "libticalcs/trunk/src/keys73.cc",
-            "libticalcs/trunk/src/keys83.cc",
-            "libticalcs/trunk/src/keys83p.cc",
-            "libticalcs/trunk/src/keys86.cc",
-            "libticalcs/trunk/src/keys89.cc",
-            "libticalcs/trunk/src/keys92p.cc",
-            "libticalcs/trunk/src/nsp_cmd.cc",
-            "libticalcs/trunk/src/nsp_rpkt.cc",
-            "libticalcs/trunk/src/nsp_vpkt.cc",
-            "libticalcs/trunk/src/probe.cc",
-            "libticalcs/trunk/src/romdump.cc",
-            "libticalcs/trunk/src/screen.cc",
-            "libticalcs/trunk/src/ticalcs.cc",
-            "libticalcs/trunk/src/tikeys.cc",
-            "libticalcs/trunk/src/type2str.cc",
-            "libticalcs/trunk/src/update.cc",
+            "backup.cc",
+            "calc_00.cc",
+            "calc_73.cc",
+            "calc_84p.cc",
+            "calc_89t.cc",
+            "calc_8x.cc",
+            "calc_9x.cc",
+            "calc_nsp.cc",
+            "calc_xx.cc",
+            "clock.cc",
+            "cmd68k.cc",
+            "cmdz80.cc",
+            "dbus_pkt.cc",
+            "dirlist.cc",
+            "dusb_cmd.cc",
+            "dusb_rpkt.cc",
+            "dusb_vpkt.cc",
+            "error.cc",
+            "keys73.cc",
+            "keys83.cc",
+            "keys83p.cc",
+            "keys86.cc",
+            "keys89.cc",
+            "keys92p.cc",
+            "nsp_cmd.cc",
+            "nsp_rpkt.cc",
+            "nsp_vpkt.cc",
+            "probe.cc",
+            "romdump.cc",
+            "screen.cc",
+            "ticalcs.cc",
+            "tikeys.cc",
+            "type2str.cc",
+            "update.cc",
         },
     });
-    libticalcs2.installHeadersDirectoryOptions(.{
-        .source_dir = upstream.path("libticalcs/trunk/src"),
-        .install_dir = .header,
-        .install_subdir = "",
+    libticalcs2.installHeadersDirectory(upstream.path("libticalcs/trunk/src"), "", .{
         .include_extensions = &.{
             "ticalcs.h",
             "export3.h",
@@ -267,5 +259,7 @@ pub fn build(b: *std.Build) void {
         },
         .exclude_extensions = &.{"/romdump.h"},
     });
-    b.installArtifact(libticalcs2);
+    const libticalcs2_install = b.addInstallArtifact(libticalcs2, .{});
+    b.step("ticalcs2", "Build libticalcs2").dependOn(&libticalcs2_install.step);
+    b.getInstallStep().dependOn(&libticalcs2_install.step);
 }
